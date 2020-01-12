44715a48a7e1d94e03174c85e34cb807ffa93b21
Steven Xia <stevenxia@protonmail.com>
2020-01-11 20:24:29 -0800

    🔧 ...

--- src/judge.py
+++ src/judge.py
@@ -1,18 +1,18 @@
 import shlex
-import sys
 import typing
 
+import display
 from judges import float_judge
 from judges import identical_judge
 from judges import default_judge
 import test
 import truncate
 
-TEST_IO_TYPE: typing.Type = typing.List[str]
-TESTCASE_TYPE: typing.Type = typing.List[typing.Tuple[TEST_IO_TYPE, TEST_IO_TYPE]]
-JUDGE_TYPE: typing.Type = typing.Callable[[typing.List[str], typing.List[str]], bool]
+IO_TYPE: typing.Type = typing.List[str]
+TESTCASE_TYPE: typing.Type = typing.List[typing.Tuple[IO_TYPE, IO_TYPE]]
+JUDGE_TYPE: typing.Type = typing.Callable[[IO_TYPE, IO_TYPE], bool]
 ANY_JUDGE: typing.Type = typing.Union[str, JUDGE_TYPE]
-TRUNCATOR_TYPE: typing.Type = typing.Callable[[typing.List[str]], typing.List[str]]
+TRUNCATOR_TYPE: typing.Type = typing.Callable[[IO_TYPE], IO_TYPE]
 
 ANSWER_CORRECT: str = "Answer Correct"
 RUNTIME_ERROR: str = "Runtime Error"
@@ -28,45 +28,40 @@ JUDGES: typing.Dict[str, JUDGE_TYPE] = {
 
 DEFAULT_TRUNCATOR: TRUNCATOR_TYPE = lambda s: truncate.truncate(s, 200, 4)
 
-SIMPLE_CHARACTERS: typing.Dict[str, str] = {
-    "⮡": ">",
-    "→": "->",
-}
-
 
 class TestcaseResult:
-    def __init__(self, given_input: TEST_IO_TYPE, given_output: TEST_IO_TYPE,
-                 received_output: TEST_IO_TYPE, error_message: TEST_IO_TYPE,
-                 exitcode: int = False, time_for_test: float = 0,
-                 time_limit_exceeded: bool = False,
-                 maximum_memory: int = 0, mem_limit_exceeded: bool = False,
-                 judge_function: ANY_JUDGE = "default"):
-        self.given_input: TEST_IO_TYPE = given_input
-        self.given_output: TEST_IO_TYPE = given_output
-        self.received_output: TEST_IO_TYPE = received_output
-        self.error_message: TEST_IO_TYPE = error_message
-        self.exitcode: int = exitcode
-        self.time_for_test: float = time_for_test
-        self.time_limit_exceeded: bool = time_limit_exceeded
-        self.maximum_memory: int = maximum_memory
-        self.mem_limit_exceeded: bool = mem_limit_exceeded
+    def __init__(self,
+                 exercise_input: IO_TYPE, exercise_output: IO_TYPE,
+                 program_stdout: IO_TYPE, program_stderr: IO_TYPE, program_exitcode: int = False,
+                 program_time: float = 0, program_tle: bool = False,
+                 program_memory: int = 0, program_mle: bool = False,
+                 judge_func: ANY_JUDGE = "default"):
+        self.exercise_input: IO_TYPE = exercise_input
+        self.exercise_output: IO_TYPE = exercise_output
+        self.program_stdout: IO_TYPE = program_stdout
+        self.program_stderr: IO_TYPE = program_stderr
+        self.program_exitcode: int = program_exitcode
+        self.program_time: float = program_time
+        self.program_tle: bool = program_tle
+        self.program_memory: int = program_memory
+        self.program_mle: bool = program_mle
 
-        if type(judge_function) == str:
-            judge_function = JUDGES[judge_function]
-        self.judge_function: JUDGE_TYPE = judge_function
+        if type(judge_func) == str:
+            judge_func = JUDGES[judge_func]
+        self.judge_func: JUDGE_TYPE = judge_func
 
         self.verdict: str = self._get_verdict()
         self.passed: bool = self.verdict == ANSWER_CORRECT
 
     def _get_verdict(self) -> str:
-        if self.time_limit_exceeded:
+        if self.program_tle:
             return TIME_LIMIT_EXCEEDED
-        elif self.mem_limit_exceeded:
+        elif self.program_mle:
             return MEM_LIMIT_EXCEEDED
-        elif self.exitcode:
+        elif self.program_exitcode:
             return RUNTIME_ERROR
         else:
-            judge_result = self.judge_function(self.received_output, self.given_output)
+            judge_result = self.judge_func(self.program_stdout, self.exercise_output)
             return ANSWER_CORRECT if judge_result else WRONG_ANSWER
 
 
@@ -74,10 +69,12 @@ class JudgeResult:
     def __init__(self) -> None:
         self.testcases: typing.List[TestcaseResult] = []
 
-        self.passed_testcases: int = 0
-        self.total_testcases: int = 0
+        self.passed: int = 0
+        self.total: int = 0
+
         self.maximum_time: float = 0.0
         self.maximum_memory: int = 0
+
         self.verdict: str = ANSWER_CORRECT
 
     def __add__(self, other: TestcaseResult) -> "JudgeResult":
@@ -93,10 +90,11 @@ class JudgeResult:
     def add_result(self, tc: TestcaseResult) -> None:
         self.testcases.append(tc)
 
-        self.passed_testcases += tc.passed
-        self.total_testcases += 1
-        self.maximum_time = max(self.maximum_time, tc.time_for_test)
-        self.maximum_memory = max(self.maximum_memory, tc.maximum_memory)
+        self.passed += tc.passed
+        self.total += 1
+
+        self.maximum_time = max(self.maximum_time, tc.program_time)
+        self.maximum_memory = max(self.maximum_memory, tc.program_memory)
 
         if self.verdict == ANSWER_CORRECT and tc.verdict != ANSWER_CORRECT:
             self.verdict = tc.verdict
@@ -106,10 +104,10 @@ def judge_file(file_command: str, testcases: TESTCASE_TYPE, time_limit: float = 
                memory_limit: int = 256, judge: JUDGE_TYPE = "default",
                truncator: TRUNCATOR_TYPE = DEFAULT_TRUNCATOR, exercise: str = "???"
                ) -> JudgeResult:
-    _display(f"Running tests for exercise: {exercise}")
-    _display(f"  ⮡ Time limit: {1000 * time_limit:.0f} ms")
-    _display(f"  ⮡ Judge: {judge}")
-    _display()
+    display.display(f"Running tests for exercise: {exercise}")
+    display.display(f"  ⮡ Time limit: {1000 * time_limit:.0f} ms")
+    display.display(f"  ⮡ Judge: {judge}")
+    display.display()
 
     result_tracker = JudgeResult()
 
@@ -128,32 +126,32 @@ def judge_file(file_command: str, testcases: TESTCASE_TYPE, time_limit: float = 
         time_taken = 1000 * min(time_limit, process_return.time_taken)
         result_tracker += TestcaseResult(
             test_input, test_output, process_output, process_errors, process_exitcode,
-            time_for_test=time_taken,
-            time_limit_exceeded=process_return.timed_out,
-            maximum_memory=process_return.max_memory,
-            mem_limit_exceeded=process_return.memory_exceeded,
-            judge_function=judge
+            program_time=time_taken,
+            program_tle=process_return.timed_out,
+            program_memory=process_return.max_memory,
+            program_mle=process_return.memory_exceeded,
+            judge_func=judge
         )
 
         this_result = result_tracker[-1]
-        _display("Case #{} → {}  [{:.0f} ms, {:.2f} MiB]".format(
+        display.display("Case #{} → {}  [{:.0f} ms, {:.2f} MiB]".format(
             test_number + 1,
             this_result.verdict,
-            this_result.time_for_test,
-            this_result.maximum_memory / 1024 / 1024
+            this_result.program_time,
+            this_result.program_memory / 1024 / 1024
         ))
 
         if this_result.verdict == RUNTIME_ERROR:
-            _display("  Error Message:")
-            _display("\n".join(f"  ⮡ {s}" for s in truncator(this_result.error_message)))
-            _display("  Exit code:")
-            _display("  ⮡ Process finished with exit code {}".format(this_result.exitcode))
+            display.display("  Error Message:")
+            display.display("\n".join(f"  ⮡ {s}" for s in truncator(this_result.program_stderr)))
+            display.display("  Exit code:")
+            display.display("  ⮡ Process finished with exit code {}".format(this_result.program_exitcode))
 
         elif this_result.verdict == WRONG_ANSWER:
-            _display("  Expected output:")
-            _display("\n".join(f"  ⮡ {s}" for s in truncator(this_result.given_output)))
-            _display("  Received output:")
-            _display("\n".join(f"  ⮡ {s}" for s in truncator(this_result.received_output)))
+            display.display("  Expected output:")
+            display.display("\n".join(f"  ⮡ {s}" for s in truncator(this_result.exercise_output)))
+            display.display("  Received output:")
+            display.display("\n".join(f"  ⮡ {s}" for s in truncator(this_result.program_stdout)))
 
     details = (
         ("{:.0f} ms, {:.2f} MiB".format(
@@ -162,26 +160,15 @@ def judge_file(file_command: str, testcases: TESTCASE_TYPE, time_limit: float = 
         ))
         if result_tracker.verdict == ANSWER_CORRECT else result_tracker.verdict
     )
-    _display("Final score: {}/{}  [{}]".format(
-        result_tracker.passed_testcases, result_tracker.total_testcases, details
+    display.display("Final score: {}/{}  [{}]".format(
+        result_tracker.passed, result_tracker.total, details
     ))
     return result_tracker
 
 
-def _encode_io(given_io: TEST_IO_TYPE) -> str:
+def _encode_io(given_io: IO_TYPE) -> str:
     return "".join(f"{input_line}\n" for input_line in given_io)
 
 
-def _decode_io(process_io: str) -> TEST_IO_TYPE:
+def _decode_io(process_io: str) -> IO_TYPE:
     return [s.strip("".join(['\r', '\n'])) for s in process_io.strip().split("\n")]
-
-
-def _display(s: str = "") -> None:
-    try:
-        sys.stdout.write(f"{s}\n")
-    except UnicodeEncodeError:
-        for old, new in SIMPLE_CHARACTERS.items():
-            s = s.replace(old, new)
-        sys.stdout.write(f"{s}\n")
-
-    sys.stdout.flush()
