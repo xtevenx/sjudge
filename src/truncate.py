2416153bacff8581c76711ea3b690b4198648b38
Steven Xia <stevenxia@protonmail.com>
2020-01-12 02:06:49 -0800

    🔧 Clean up truncate() for release.

--- src/truncate.py
+++ src/truncate.py
@@ -1,29 +1,44 @@
+"""
+This module manages the truncation of messages for cleaner display.
+"""
+
 import typing
 
-INFINITY: int = (1 << 31) - 1
-TRUNCATED_STRING: str = "<output truncated>"
+# a large number to represent no limit
+INFINITY: int = (1 << 64) - 1
+
+# the message to display when something has been truncated
+TRUNCATED_STRING: str = "⯇truncated⯈"
 
 
-def truncate(s: typing.List[str], character_limit: typing.Optional[int],
-             newline_limit: typing.Optional[int]) -> typing.List[str]:
-    if character_limit is None and newline_limit is None:
+def truncate(s: typing.List[str], char_limit: typing.Optional[int] = None,
+             nl_limit: typing.Optional[int] = None) -> typing.List[str]:
+    """
+    Truncate a string based on a given character and newline limit.
+    :param s: string to truncate
+    :param char_limit: maximum characters in the truncated string
+    :param nl_limit: maximum newlines in the truncated string
+    :return: truncated string (based on `char_limit` and `nl_limit`)
+    """
+
+    if char_limit is None and nl_limit is None:
         return s
 
-    character_limit = INFINITY if character_limit is None else character_limit
-    newline_limit = INFINITY if newline_limit is None else newline_limit
+    char_limit = INFINITY if char_limit is None else char_limit
+    nl_limit = INFINITY if nl_limit is None else nl_limit
 
     return_s: typing.List[str] = []
     for line in s:
-        if len(line) > character_limit:
-            return_s.append(line[:character_limit])
+        if len(line) > char_limit:
+            return_s.append(line[:char_limit])
             return_s.append(TRUNCATED_STRING)
             break
-        elif newline_limit <= 0:
+        elif nl_limit <= 0:
             return_s.append(TRUNCATED_STRING)
             break
-        else:
-            return_s.append(line)
-            character_limit -= len(line)
-            newline_limit -= 1
+
+        return_s.append(line)
+        char_limit -= len(line)
+        nl_limit -= 1
 
     return return_s
