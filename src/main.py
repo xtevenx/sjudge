3857ec8a7609454b48b295cc6de36f14deae7314
Steven Xia <stevenxia@protonmail.com>
2020-01-12 17:58:18 -0800

    ➖ Simplify main script.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 import argparse
-import json
 import os
 import sys
 import traceback
 
 import command
+import display
 import exercise
 import judge
 
@@ -26,64 +26,51 @@ def main():
         "-s", "--see_description", action="store_true",
         help="display the description for the given exercise.", dest="see_description")
     parser.add_argument(
-        "-e", "--exercises_location", action="store",
+        "-e", "--exercises_location", action="store", default=DEFAULT_EXERCISES,
         help="set the location of the exercises.", dest="exercises_location")
     arguments = parser.parse_args()
 
-    exercises_location = (
-        arguments.exercises_location if arguments.exercises_location is not None
-        else DEFAULT_EXERCISES
-    )
-
     if arguments.list_exercises:
-        lessons_list = exercise.list_exercises(exercises_location)
-        lessons_list = [f"  - {lesson_name}" for lesson_name in lessons_list]
+        lessons_list = exercise.list_exercises(arguments.exercises_location)
+        lessons_list = [f"  ⮡ {lesson_name}" for lesson_name in lessons_list]
 
-        print(f"Found {len(lessons_list)} lessons:")
-        print("\n".join(lessons_list))
+        display.display(f"Found {len(lessons_list)} lessons:")
+        display.display("\n".join(lessons_list))
         sys.exit(0)
 
     if arguments.see_description:
         if arguments.exercise_name is None:
-            print("error: no exercise name specified")
-        else:
-            try:
-                print(exercise.get_description(
-                    exercises_location, arguments.exercise_name
-                ))
-            except FileNotFoundError:
-                print(f"error: exercise `{arguments.exercise_name}` does not exist.")
+            raise AssertionError("no exercise name was specified")
 
+        display.display(exercise.get_description(
+            arguments.exercises_location, arguments.exercise_name
+        ))
         sys.exit(0)
 
     if arguments.exercise_name is None or arguments.program_path is None:
         parser.print_help()
-        sys.exit(0)
 
-    exercise_test_path = os.path.join(exercises_location, f"{arguments.exercise_name}.json")
-    if not os.path.isfile(exercise_test_path):
-        print(f"error: exercise `{arguments.exercise_name}` does not exist.")
-        sys.exit(0)
+        missing_value = "exercise_name" if arguments.exercise_name is None else "program_path"
+        raise AssertionError(f"the argument `{missing_value}` is missing")
 
     if not os.path.isfile(arguments.program_path):
-        print(f"error: program `{arguments.program_path}` does not exist.")
-        sys.exit(0)
+        raise AssertionError(f"the file `{arguments.program_path}` does not exist")
 
     judge.judge_program(
         command.get_command(arguments.program_path),
-        **json.load(open(exercise_test_path, "r"))
+        **exercise.get_specs(arguments.exercises_location, arguments.exercise_name)
     )
 
 
 if __name__ == "__main__":
     try:
         main()
-    except SystemExit as err:
-        sys.exit(*err.args)
-    except KeyboardInterrupt:
-        print("stopping judging due to user interrupt.")
     except AssertionError as err:
         print(f"error: {err.args[0]}.")
+    except KeyboardInterrupt:
+        print("stopping judging due to user interrupt.")
+    except SystemExit as err:
+        sys.exit(*err.args)
     except BaseException as err:
         print("<-- ERROR TRACEBACK -->")
         traceback.print_tb(err.__traceback__)
