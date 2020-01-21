f76f93cf25b07e055331bfe733d23084c0bdfccc
Steven Xia <stevenxia@protonmail.com>
2020-01-20 16:18:02 -0800

    🛠️ Fix bug in command tests.
    Windows test systems don't have java installed so... yea.

--- tests/command_test.py
+++ tests/command_test.py
@@ -7,6 +7,8 @@ source_dir = os.path.join(parent_dir, "src/")
 sys.path.append(source_dir)
 
 import platform
+import pytest
+
 from command import _exists
 from command import get_command
 
@@ -22,10 +24,15 @@ def test__get_command():
     assert get_command("main") == "./main"
     assert get_command("main.exe") == "./main.exe"
 
-    assert get_command("main.jar") == "java -jar main.jar"
-
     c = "python main.pyc" if platform.system() == WINDOWS else "python3 main.pyc"
     assert get_command("main.pyc") == c
 
     c = "python main.py" if platform.system() == WINDOWS else "python3 main.py"
     assert get_command("main.py") == c
+
+    # test for `get_command` failure.
+    import command
+    command.LANGUAGES[frozenset({"test"})] = ("testtesttest",) * 2
+
+    with pytest.raises(AssertionError):
+        assert get_command("main.test")
